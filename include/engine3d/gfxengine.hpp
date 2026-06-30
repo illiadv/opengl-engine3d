f6e90d57f196ca8915f861239dde9edf57954176
illiadv <illiadanylov.5@gmail.com>
2026-06-30 14:03:08 +0200

    Remove extra includes

--- include/engine3d/gfxengine.hpp
+++ include/engine3d/gfxengine.hpp
@@ -1,9 +1,6 @@
 #ifndef GFXENGINE_HPP
 #define GFXENGINE_HPP
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
