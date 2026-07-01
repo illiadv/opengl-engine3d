b880fc7746fad097cb44ad00d6f848e861f36300
illiadv <illiadanylov.5@gmail.com>
2026-07-01 15:54:59 +0200

    Add new texture classes

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,19 +1,86 @@
-#include "texture.hpp"
+#include <stdexcept>
+#include <cstring>
 
-unsigned int CreateTexture(const char* filename, int wrap)
+#include "texture.hpp"
+#include "util.hpp"
+#include "stb_image.h"
+
+unsigned char *LoadTextureFile(const char *path, int &width, int &height, int &nChannels)
 {
-    int width, height, nChannels;
-    unsigned char *textureData = stbi_load(filename, &width, &height, &nChannels, 0);
+    unsigned char *textureData = stbi_load(path, &width, &height, &nChannels, 0);
 
     if (!textureData)
     {
-	printf("Failed to load texture %s\n", filename);
-	return 0;
+	throw (std::runtime_error(std::string("Failed to load texture") + path + "\n"));
     }
-    else
+
+    printf("Loaded texture %s %dx%d, %d channels\n", path, width, height, nChannels);
+    return textureData;
+}
+
+GLint GetSourceFormat(int nChannels)
+{
+    switch (nChannels) {
+	case 1:
+	    return GL_RED;
+	case 3:
+	    return GL_RGB;
+	default:
+	    return GL_RGBA;
+    }
+}
+
+Texture2D::Texture2D(const char *path)
+{
+    int width, height, nChannels;
+    unsigned char *textureData = LoadTextureFile(path, width, height, nChannels);
+
+    unsigned int sourceFormat = GetSourceFormat(nChannels);
+
+    glGenTextures(1, &m_ID);
+
+    glBindTexture(GL_TEXTURE_2D, m_ID);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, sourceFormat, width, height, 0, sourceFormat, GL_UNSIGNED_BYTE, textureData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(textureData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::Bind(uint32_t slot)
+{
+    glCall(glActiveTexture(GL_TEXTURE0 + slot));
+    // glCall(glUniform1i(glGetUniformLocation(shader, name.c_str()), i));
+    glCall(glBindTexture(GL_TEXTURE_2D, m_ID));
+}
+
+unsigned int CreateTextureArray(std::initializer_list<const char*> filenames)
+{
+    unsigned char *texArray = nullptr;
+    int width, height, nChannels;
+
+    int i = 0;
+    for (auto filename : filenames)
     {
-	printf("Loaded texture %s %dx%d, %d channels\n", filename, width, height, nChannels);
+	// NOTE: for some files it may be necessary to force certain nChannels (e.g. 4)
+	unsigned char *textureData = LoadTextureFile(filename, width, height, nChannels);
+
+	if (texArray == nullptr)
+	{
+	    texArray = new unsigned char[width * height * nChannels * filenames.size()];
+	}
+	
+	memcpy(texArray + (width * height * nChannels) * i, textureData, width * height * nChannels);
+	stbi_image_free(textureData);
+	i++;
     }
+
 
     unsigned int sourceFormat = 0;
     switch (nChannels) {
@@ -32,23 +99,40 @@ unsigned int CreateTexture(const char* filename, int wrap)
     }
 
     unsigned int texture;
-    glGenTextures(1, &texture);
+    glCall(glGenTextures(1, &texture));
 
 
-    glBindTexture(GL_TEXTURE_2D, texture);
+    glCall(glBindTexture(GL_TEXTURE_2D_ARRAY, texture));
 
-    // how exactly does this work?
-    // afair it works for the bound one only
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);	
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    
+    glCall(glTexImage3D(GL_TEXTURE_2D_ARRAY,
+		 0,                 // mipmap level
+		 GL_RGBA8,          // gpu texel format
+		 width,             // width
+		 height,            // height
+		 filenames.size(),  // depth
+		 0,                 // border
+		 GL_RGBA,           // cpu pixel format
+		 GL_UNSIGNED_BYTE,  // cpu pixel coord type
+		 texArray));        // pixel data
+    glCall(glGenerateMipmap(GL_TEXTURE_2D_ARRAY));
 
-    glTexImage2D(GL_TEXTURE_2D, 0, /*GL_RGB*/ sourceFormat, width, height, 0, sourceFormat, GL_UNSIGNED_BYTE, textureData);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    stbi_image_free(textureData);
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glCall(glBindTexture(GL_TEXTURE_2D_ARRAY, 0));
+
+    if (texArray != nullptr)
+	delete[] texArray;
 
     return texture;
+}
+
+void TextureArray2D::Bind(uint32_t slot)
+{
+    glCall(glActiveTexture(GL_TEXTURE0 + slot));
+    // glCall(glUniform1i(glGetUniformLocation(shader, name.c_str()), i));
+    glCall(glBindTexture(GL_TEXTURE_2D_ARRAY, m_ID));
 }
--- src/texture.hpp
+++ src/texture.hpp
@@ -1,10 +1,33 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-// #include <GL/glew.h>
 #include <glad/glad.h>
-#include "stb_image.h"
+#include <initializer_list>
 
-unsigned int CreateTexture(const char* filename, int wrap = GL_REPEAT);
+class Texture
+{
+public:
+    virtual ~Texture() = default;
+    virtual void Bind(uint32_t slot = 0) = 0;
+};
+
+class Texture2D : public Texture
+{
+public:
+    Texture2D(const char *path);
+    void Bind(uint32_t slot = 0) override;
+private:
+    GLuint m_ID;
+};
+
+class TextureArray2D : public Texture
+{
+public:
+    TextureArray2D(const char *path);
+    TextureArray2D(std::initializer_list<const char *> pathList);
+    void Bind(uint32_t slot = 0) override;
+private:
+    GLuint m_ID;
+};
 
 #endif
