d74f8de630ff5d58d4a40438661a283ddb3dcf45
illiadv <illiadanylov.5@gmail.com>
2026-07-02 11:42:43 +0200

    Store uniforms and texture data in materials, set on bind

--- include/engine3d/material.hpp
+++ include/engine3d/material.hpp
@@ -1,18 +1,41 @@
-#ifndef OBJECT_HPP
-#define OBJECT_HPP
-#include <glad/glad.h>
+#ifndef MATERIAL_HPP
+#define MATERIAL_HPP
+
+#include <string>
+#include <unordered_map>
+
+#include "glm/ext/vector_float3.hpp"
+#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+
+#include "texture.hpp"
 
 
 class Material
 {
 public:
-    Material(const char *vertexShaderSource, const char *fragmentShaderSource, float shininess);
+    Material(const char *vertexShaderSource, const char *fragmentShaderSource);
+    Material(unsigned int shaderProgram);
+
+    void SetTexture(std::string name, Texture *texture, uint32_t slot);
+    void uSetFloat(std::string name, float f);
+    void uSetVec3(std::string name, glm::vec3 v);
+
+    void Bind() const;
+
     const unsigned int& GetShaderProgram() const;
 
-    float shininess = 0;
 private:
-    unsigned int shader = 0;
+    unsigned int m_shader = 0;
+    std::unordered_map<std::string, float> m_floats;
+    std::unordered_map<std::string, glm::vec3> m_vec3s;
+
+    struct TextureData {
+	Texture *texture;
+	uint32_t slot;
+    };
+
+    std::unordered_map<std::string, TextureData> m_textures;
 };
 
 #endif
--- src/material.cpp
+++ src/material.cpp
@@ -1,25 +1,58 @@
 #include "engine3d/material.hpp"
+#include "glm/gtc/type_ptr.hpp"
 #include "shader.hpp"
 #include "util.hpp"
 
-Material::Material(const char *vertexShaderSource, const char *fragmentShaderSource, float shininess)
+Material::Material(const char *vertexShaderSource, const char *fragmentShaderSource)
 {
-    shader = CreateShader(vertexShaderSource, fragmentShaderSource);
-    this->shininess = shininess;
+    m_shader = CreateShader(vertexShaderSource, fragmentShaderSource);
 
-    unsigned int blockIndex = glGetUniformBlockIndex(shader, "Matricies");
+    unsigned int blockIndex = glGetUniformBlockIndex(m_shader, "Matricies");
     glCheckError();
-    glUniformBlockBinding(shader, blockIndex, 0);
-    glCheckError();
-
-    blockIndex = glGetUniformBlockIndex(shader, "Lights");
-    glCheckError();
-    glUniformBlockBinding(shader, blockIndex, 1);
+    glUniformBlockBinding(m_shader, blockIndex, 0);
     glCheckError();
 
+    blockIndex = glGetUniformBlockIndex(m_shader, "Lights");
+    glCheckError();
+    glUniformBlockBinding(m_shader, blockIndex, 1);
+    glCheckError();
+
+}
+void Material::uSetFloat(std::string name, float f)
+{
+    m_floats[name] = f;
+}
+
+void Material::uSetVec3(std::string name, glm::vec3 v)
+{
+    m_vec3s[name] = v;
+}
+
+void Material::SetTexture(std::string name, Texture *texture, uint32_t slot)
+{
+    m_textures[name] = {texture, slot};
+}
+
+void Material::Bind() const
+{
+    for (auto& [name, value] : m_floats)
+    {
+	SetFloat(m_shader, name.c_str(), value);
+    }
+
+    for (auto& [name, value] : m_vec3s)
+    {
+	SetVec3(m_shader, name.c_str(), glm::value_ptr(value));
+    }
+
+    for (auto& [name, textureData] : m_textures)
+    {
+	textureData.texture->Bind(textureData.slot);
+	SetInt(m_shader, name.c_str(), textureData.slot);
+    }
 }
 
 const unsigned int& Material::GetShaderProgram() const
 {
-    return shader;
+    return m_shader;
 }
