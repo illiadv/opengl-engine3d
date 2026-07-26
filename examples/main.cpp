42a45ffcc484a0d9b658b03b5684fe32de70c933
illiadv <illiadanylov.5@gmail.com>
2026-07-26 15:36:35 +0200

    Update example

--- examples/main.cpp
+++ examples/main.cpp
@@ -123,6 +123,8 @@ int main()
 
     DirectionalLight dirLight(glm::vec3(-0.2f, -0.5f, -1.0f), glm::vec3(0.2f), glm::vec3(0.7f), glm::vec3(0.5f));
     PointLight pointLight(glm::vec3(0.0f, 5.0f, 3.0f), glm::vec3(1.0f), glm::vec3(1.0f, 0.8f, 0.0f), glm::vec3(0.5f), 50);
+
+    camera.position = glm::vec3(1.5f, 0.5f, 4.0f);
     
     float deltaTime = 0.0f; // Time between current frame and last frame
     float lastFrame = 0.0f; // Time of last frame
