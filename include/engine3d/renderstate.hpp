5445399ff5e658eaf402343c204d6c05339dbd24
illiadv <illiadanylov.5@gmail.com>
2026-08-10 15:30:19 +0200

    Add operator==

--- include/engine3d/renderstate.hpp
+++ include/engine3d/renderstate.hpp
@@ -1,3 +1,6 @@
+#ifndef RENDERSTATE_HPP
+#define RENDERSTATE_HPP
+
 enum class BlendMode { None, Alpha };
 enum class CullMode { None, Back };
 
@@ -6,4 +9,13 @@ struct RenderState
     bool depthTesting = true;
     BlendMode blending = BlendMode::None;
     CullMode culling = CullMode::Back;
+
+    bool operator==(const RenderState &other) const
+    {
+	return (depthTesting == other.depthTesting) &&
+	    (blending == other.blending) &&
+	    (culling == other.culling);
+    }
 };
+
+#endif
